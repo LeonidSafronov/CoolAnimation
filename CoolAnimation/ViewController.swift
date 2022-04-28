c21dc29650ad6466d176141f6b46895f3d053d40
LeonidSafronov <safronovleonid@gmail.com>
2022-04-28 09:18:06 +0300

    Initial Commit

---
+++ CoolAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoolAnimation
+//
+//  Created by Leonid Safronov on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
